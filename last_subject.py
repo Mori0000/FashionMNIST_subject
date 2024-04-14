9964b8f07f81adb0adb8e3fba5630e03848143d5
Mori0000 <sd21092@tti-j.net>
2024-04-14 22:07:33 +0900

    try_efficientnet

--- last_subject.py
+++ last_subject.py
@@ -3,59 +3,66 @@ from torchvision import datasets, transforms
 import torch.nn as nn
 import torch.optim as optim
 import time
+import timm  # Import timm
 
-# import my tools 
-from tools_last.model import CustomNetwork
+from torchvision.transforms import Lambda
+
+# Data preprocessing
+transform = transforms.Compose([
+    transforms.Resize(224),  # Resize to the input size expected by EfficientNet
+    transforms.Grayscale(num_output_channels=3),  # Convert 1-channel grayscale to 3-channel grayscale
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))  # Normalize for three channels
+])
 
 
-# preprocessing data
-transform = transforms.Compose([transforms.ToTensor(),
-                                transforms.Normalize((0.5,), (0.5,))])
-
-# load data
+# Load data
 trainset = datasets.FashionMNIST('~/.pytorch/F_MNIST_data/', download=True, train=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=32, shuffle=True)  # Adjusted batch size for GPU memory considerations
 testset = datasets.FashionMNIST('~/.pytorch/F_MNIST_data/', download=True, train=False, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=True)
+testloader = torch.utils.data.DataLoader(testset, batch_size=32, shuffle=True)
 
-# data on GPU
+# Set device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# model instance
-model = CustomNetwork()
+# Load EfficientNet model
+model = timm.create_model('efficientnet_b0', pretrained=True)
+model.classifier = nn.Linear(model.classifier.in_features, 10)  # Adjust the classifier
 
-# model on GPU
+# Wrap the model with DataParallel
+if torch.cuda.device_count() > 1:
+    print(f"Using {torch.cuda.device_count()} GPUs!")
+    model = nn.DataParallel(model)
+
+# Move model to the device
 model.to(device)
 
-# loss function and optimizer
+# Loss function and optimizer
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=0.003)
+optimizer = optim.Adam(model.parameters(), lr=0.001)
 
-# train model
+# Train the model
 epochs = 10
 for epoch in range(epochs):
     running_loss = 0
     for images, labels in trainloader:
         images, labels = images.to(device), labels.to(device)
         optimizer.zero_grad()
-        log_ps = model(images)
-        loss = criterion(log_ps, labels)
+        outputs = model(images)
+        loss = criterion(outputs, labels)
         loss.backward()
         optimizer.step()
-        
+
         running_loss += loss.item()
-    else:
-        print(f"Epoch {epoch+1}/{epochs}.. "
-              f"Train loss: {running_loss/len(trainloader):.3f}")
+    print(f"Epoch {epoch+1}/{epochs}.. Train loss: {running_loss/len(trainloader):.3f}")
 
-
-# evaluate model
+# Evaluate the model
 correct = 0
 total = 0
-model.eval()  # Put the model in evaluation mode
+model.eval()  # Evaluation mode
 with torch.no_grad():
     for images, labels in testloader:
-        images, labels = images.to(device), labels.to(device)  # Move data to the device
+        images, labels = images.to(device), labels.to(device)
         outputs = model(images)
         _, predicted = torch.max(outputs.data, 1)
         total += labels.size(0)
@@ -63,6 +70,6 @@ with torch.no_grad():
 
 print(f'Accuracy of the network on the 10000 test images: {100 * correct / total}%')
 
+# Save the model
 now = time.strftime("%Y%m%d_%H%M%S")
-# save model
-torch.save(model.state_dict(), f'./run/fashion_model_{now}.pth')
+torch.save(model.module.state_dict(), f'./run/fashion_efficientnet_{now}.pth')

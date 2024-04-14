bc8d341401177966f0ea161dc3dfffb8e0ab8e4c
Mori0000 <sd21092@tti-j.net>
2024-04-14 20:59:57 +0900

    change_models

--- tools_last/model.py
+++ tools_last/model.py
@@ -4,13 +4,23 @@ from torch import nn
 class CustomNetwork(nn.Module):
     def __init__(self):
         super(CustomNetwork, self).__init__()
-        self.fc1 = nn.Linear(28 * 28, 512)
-        self.fc2 = nn.Linear(512, 256)
-        self.fc3 = nn.Linear(256, 10)
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)  # input channel is 1, output channels is 32
+        self.bn1 = nn.BatchNorm2d(32)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.pool = nn.MaxPool2d(2, 2)  # 2x2 max pooling
+        self.dropout1 = nn.Dropout(0.25)
+        self.fc1 = nn.Linear(64 * 7 * 7, 128)  # after 2 pooling layers, the image size is reduced to 7x7
+        self.bn3 = nn.BatchNorm1d(128)
+        self.dropout2 = nn.Dropout(0.5)
+        self.fc2 = nn.Linear(128, 10)
 
     def forward(self, x):
-        x = x.view(x.shape[0], -1)  # Flatten the image
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = self.pool(F.relu(self.bn2(self.conv2(x))))
+        x = self.dropout1(x)
+        x = x.view(-1, 64 * 7 * 7)  # Flatten the image
+        x = F.relu(self.bn3(self.fc1(x)))
+        x = self.dropout2(x)
+        x = self.fc2(x)
         return x
